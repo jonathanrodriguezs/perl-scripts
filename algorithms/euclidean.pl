670d05139587cbbc9cc4f6719c40fac1b0843892
Jonathan Rodriguez <jonathan.rodriguez.sanchez@u-erre.mx>
2020-06-28 02:03:31 -0500

    chore: minor changes

--- algorithms/euclidean.pl
+++ algorithms/euclidean.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
 # euclidean algo ($a, $b) to compute the MCD of $a and $b
 sub euclidean_mcd {
     my ($a, $b) = @_;
     return euclidean_mcd($b, $a) if ($a < $b);
-
     while($b != 0) {
         ($a, $b) = ($b, $a % $b);
     }
-
     return $a;
 }
 
--- algorithms/factorial.pl
+++ algorithms/factorial.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
 # factorial recursive ($n) computes the factorial of $n
 # using a recursive algorithm.
